7be710f55fdba610c8131f3ac0cd53631e77b8ec
Gregg Meluski <gmeluski@hotmail.com>
2016-03-24 21:53:43 -0400

    begin the insert statement

--- src/heap.js
+++ src/heap.js
@@ -1,7 +1,22 @@
 export default class Heap {
   constructor () {
-    this.stack = []
+    this.stack = [0]
+    this.heapCount = 0
   }
+
+  get list () {
+    return this.stack
+  }
+
+  percolateUp(index) {
+
+
+  }
+
+  insert(value) {
+    this.stack
+  }
+
 
 }
 
--- test/heap.js
+++ test/heap.js
@@ -1,9 +1,26 @@
 import {expect} from "chai"
-import heap from "../src/heap"
+import Heap from "../src/heap"
 
 describe("the heap module", () => {
+  let heap
+
+  beforeEach(() => {
+    heap = new Heap()
+  })
+
   it("is a class", () => {
-    expect(heap).to.be.a.class
+    expect(Heap).to.be.a.class
+  })
+
+  it("places elements correctly", () => {
+    let finalHeap = [0, 5, 9, 11, 14]
+
+    heap.insert(14)
+    heap.insert(9)
+    heap.insert(11)
+    heap.insert(5)
+
+    expect(heap.list).to.eql(finalHeap)
   })
 })
 
