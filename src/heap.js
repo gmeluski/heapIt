c477ea7ce99d8af4cc1b3d2eaf9ab12e62b78b60
Gregg Meluski <gmeluski@hotmail.com>
2016-03-28 21:48:20 -0400

    adding in percolateUp and insert commands

--- src/heap.js
+++ src/heap.js
@@ -8,13 +8,39 @@ export default class Heap {
     return this.stack
   }
 
-  percolateUp(index) {
+  getParentIndex(i) {
+    return Math.floor(i / 2)
+  }
 
+  swapPositions(parentIndex, childIndex) {
+    let placeHolder = this.stack[parentIndex]
+
+    this.stack[parentIndex] = this.stack[childIndex]
+    this.stack[childIndex] = placeHolder
+  }
+
+  percolateUp(index) {
+    let parentIndex = this.getParentIndex(index)
+    let currentIndex = index
+    let placeHolder
+
+    while (parentIndex  > 0) {
+      // if the current index is less than the parent, swap
+      if (this.stack[currentIndex] < this.stack[parentIndex]) {
+        this.swapPositions(parentIndex, currentIndex)
+      }
+
+      // make the parentIndex the currentIndex and find the parentIndex
+      currentIndex = parentIndex
+      parentIndex = this.getParentIndex(currentIndex)
+    }
 
   }
 
   insert(value) {
-    this.stack
+    this.stack.push(value)
+    this.heapCount++
+    this.percolateUp(this.heapCount)
   }
 
 
